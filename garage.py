4cf7163e91afe5827b1783f7909f888d6c11d143
gabyslaw <gabyslaw@gmail.com>
2022-06-05 21:07:54 +0100

    added the delete method

--- garage.py
+++ garage.py
@@ -59,5 +59,15 @@ def updatecar(id):
         conn.close()
         return redirect('/')
 
+@garage.route('/deletecar/<int:id>')
+def deletecar(id):
+    conn = connection()
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM LiverPoolCars WHERE id = %s", (str(id)))
+    conn.commit()
+    conn.close()
+    return redirect('/')
+    
+
 if __name__ == '__main__':
     garage.run(debug=True)
